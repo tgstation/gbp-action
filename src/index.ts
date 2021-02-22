8455b79fdfeb09848060283fb9020d32b874b80f
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2021-02-22 15:18:02 -0800

    context.issue

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core"
 import * as github from "@actions/github"
 
 async function run() {
-    console.log(JSON.stringify(github.context.payload))
+    console.log(JSON.stringify(github.context.issue))
 }
 
 run()
