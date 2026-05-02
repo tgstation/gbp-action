50dc1066d154ab0b120785b0206953f665ae889a
SyncIt21 <VLord3D@gmail.com>
2026-05-03 00:48:39 +0530

    roll up on this b****

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'out/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve()
+  ]
+};
